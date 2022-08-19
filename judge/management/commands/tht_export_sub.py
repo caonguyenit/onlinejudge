3e8c5adb72d413e178a90037f9f0ad5086cbf108
Le Duy Thuc <leduythuc.cs@gmail.com>
2022-08-19 14:42:05 +0000

    only export latest sub

--- judge/management/commands/tht_export_sub.py
+++ judge/management/commands/tht_export_sub.py
@@ -3,7 +3,6 @@ import os
 from django.core.management.base import BaseCommand, CommandError
 
 from judge.models import Contest, ContestParticipation
-from judge.models.submission import Submission
 
 
 class Command(BaseCommand):
@@ -21,9 +20,6 @@ class Command(BaseCommand):
         print(contest)
         if contest is None:
             raise CommandError('contest not found')
-
-        if os.path.exists(output_file):
-            raise CommandError('output file already exists')
 
         users = contest.users.filter(virtual=ContestParticipation.LIVE).select_related('user__user')
         problems = contest.contest_problems.order_by('order')
@@ -45,17 +41,27 @@ class Command(BaseCommand):
 
             username = user.user.user.username
 
-            submissions = user.submissions.exclude(submission__result__in=('IE', 'CE'))
+            submissions = user.submissions.exclude(submission__result__in=('IE', 'CE')).order_by('-submission__id')
+
             result = {
 
             }
+
             for submission in submissions:
                 cases = submission.submission.test_cases.order_by('case')
                 result_string = ''.join(['1' if case.status == 'AC' else '0' for case in cases])
+
+                if submission.submission.problem.id in result:
+                    # skip
+                    continue
+
                 result[submission.submission.problem.id] = result_string
+
             row = [username]
+
             for problem in problems:
                 row.append('"' + result.get(problem.problem.id, '0') + '"')
+
             rows.append(','.join(row))
 
         open(output_file, 'w').write('\n'.join(rows))
